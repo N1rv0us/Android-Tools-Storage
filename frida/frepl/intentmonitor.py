3ff26dc81cb95c249ae595a647f31c6056febf08
n1rv0us <listennter@gmail.com>
2021-08-06 11:06:33 +0800

    update intent monitor unit

--- frida/frepl/intentmonitor.py
+++ frida/frepl/intentmonitor.py
@@ -18,3 +18,55 @@ class IntentMonitor(AppAgent):
     def my_message_handler(self, message, payload):
         return super().my_message_handler(message, payload)
 
+
+    # monitor package list set here
+    def set_all_src(self,packages) : 
+        self.src = set(packages)
+        self._api.src(self.src)
+
+    def add_src_package(self,package) : 
+        self.src.add(package)
+        self._api.src(self.src)
+
+    def remove_src_package(self,package):
+        try :
+            self.src.remove(package)
+            self._api.src(self.src)
+            return True
+        except:
+            return False
+
+    def get_src_list(self):
+        return list(self.src)
+
+    def set_all_dst(self,packages):
+        self.dst = set(packages)
+        self._api.dst(self.dst)
+
+    def add_dst_package(self,package):
+        self.dst.add(package)
+        self._api.dst(self.dst)
+
+    def remove_dst_package(self,package):
+        try:
+            self.dst.remove(package)
+            self._api.dst(self.dst)
+            return True
+        except:
+            return False
+
+    def get_dst_list(self):
+        return list(self.dst)
+
+    ## monitor function launch here
+    def launch_activity(self):
+        self._api.activity()
+
+    def launch_service(self):
+        self._api.service()
+
+    def launch_broadcast(self):
+        self._api.broadcast()
+
+    def launch_provider(self):
+        self._api.provider()
